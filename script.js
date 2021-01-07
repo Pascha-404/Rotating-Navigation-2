95f17e8ffd3b1d6cce93989ae938a875a24cea49
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-07 20:18:39 +0100

    cleaned code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const openBtn = document.querySelector('#open');
 const closeBtn = document.querySelector('#close');
+const body = document.querySelector('body');
 
 openBtn.addEventListener('click', () => {
     container.classList.add('showNav');
